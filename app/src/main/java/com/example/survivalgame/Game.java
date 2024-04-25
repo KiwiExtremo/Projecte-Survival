fbdaa498138cd3d7ff059993cc749a04d526a32b
KiwiExtremo <thearielcompany@gmail.com>
2024-04-25 21:02:44 +0200

    Updated things

---
+++ app/src/main/java/com/example/survivalgame/Game.java
@@ -0,0 +1,9 @@
+package com.example.survivalgame;
+
+import android.view.View;
+
+public class Game extends View {
+    public Game(MainActivity mainActivity) {
+        super();
+    }
+}
---
+++ app/src/main/java/com/example/survivalgame/User.java
@@ -0,0 +1,2 @@
+package com.example.survivalgame;public class User {
+}
